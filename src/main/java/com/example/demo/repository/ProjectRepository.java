73ccb2a59c711fcdc58e0942c1cc2796b74cdd85
smartHedgehog-dev <elarionovnof@gmail.com>
2026-03-24 22:00:00 +0400

    Удалены неиспользуемые методы обновления полей через запросы

--- src/main/java/com/example/demo/repository/ProjectRepository.java
+++ src/main/java/com/example/demo/repository/ProjectRepository.java
@@ -6,14 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectRepository extends JpaRepository<Project, String> {
-    @Modifying(clearAutomatically = true)
-    @Query(value = "UPDATE projects SET ?2 = ?3 WHERE id = ?1", nativeQuery = true)
-    int updateFieldById(int id, String fieldName, String newValue);
-
-    @Modifying(clearAutomatically = true)
-    @Query(value = "UPDATE projects SET ?2 = ?3 WHERE name = ?1", nativeQuery = true)
-    int updateFieldByName(String name, String fieldName, String newValue);
-
     Project findById(int id);
     Project findByName(String name);
     Project deleteById(int id);
--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -6,15 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
-    //Чтобы очистить контекст после выполнения запроса, иначе при последующем чтении можно получить устаревшие данные
-    @Modifying(clearAutomatically = true)
-    @Query(value = "UPDATE tasks SET ?2 = ?3 WHERE id = ?1", nativeQuery = true)
-    int updateFieldById(int id, String fieldName, String newValue);
-
-    @Modifying(clearAutomatically = true)
-    @Query(value = "UPDATE tasks SET ?2 = ?3 WHERE name = ?1", nativeQuery = true)
-    int updateFieldByName(String name, String fieldName, String newValue);
-
     Task findById(int id);
     Task findByName(String name);
     Task deleteById(int id);
